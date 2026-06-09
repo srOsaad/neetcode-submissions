61bdd158a90b15a1c383ed3b5cf61b0ac1ef9d85
Shahryar Rahman Saad <135040634+srOsaad@users.noreply.github.com>
2026-06-09 22:06:30 +0600

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAChar(self,c: char) -> bool:
+        return (ord(c)>=ord('A') and ord(c)<=ord('Z')) or (ord(c)>=ord('a') and ord(c)<=ord('z')) or (ord(c)>=ord('0') and ord(c)<=ord('9'))
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s)-1
+        while i<len(s) and not self.isAChar(s[i]):
+            i+=1
+        while j>=0 and not self.isAChar(s[j]):
+            j-=1
+           # print(j,s[j])
+
+        while i!=j and i<j:
+            if s[i].lower()!=s[j].lower():
+                #print('[',s[i],'] [',s[j],']',i,j)
+                return False
+            i+=1
+            j-=1
+            while i<len(s) and not self.isAChar(s[i]):
+                i+=1
+            while j>=0 and not self.isAChar(s[j]):
+                j-=1
+    
+        return True
